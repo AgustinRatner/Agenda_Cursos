2649013a449ea2f046906edc9d3bddc8c2a9a2d3
AgustinRatner <agustinratsk8@gmail.com>
2023-07-29 21:01:24 -0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const btnAdd = document.querySelector('[data-form-btn]');
 
-btnAdd.addEventListener("click", (evento) => {
+const createTask_add = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
   console.log(input.value);
-})
+  input.value='';
+}
+
+btnAdd.addEventListener("click",createTask_add);
