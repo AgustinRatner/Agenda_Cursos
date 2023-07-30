e26f20676ea144798b13543b2cfa013f1a163920
AgustinRatner <agustinratsk8@gmail.com>
2023-07-29 21:29:10 -0300

    Clase 3 terminada

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const btnAdd = document.querySelector('[data-form-btn]');
 const createTask_add = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
-  console.log(input.value);
+  const list=document.querySelector('[data-list]');
+  const task=document.createElement('li');
+  task.classList.add('card');
+
+  const content = 
+  `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${input.value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+  task.innerHTML = content;
+  list.appendChild(task);
   input.value='';
 }
 
